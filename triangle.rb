ad3661a9905c86711f0b160ed599522ff9999481
santacos <santacos_lala@hotmail.com>
2018-05-03 23:23:33 +0700

    [24] Triangle project

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,19 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if !allSideHasValidLength?(a, b, c)
+    raise TriangleError
+  elsif a == b && a == c
+    return :equilateral
+  elsif a == b || a == c || b == c
+    return :isosceles
+  else
+    return :scalene
+  end
+end
+
+def allSideHasValidLength?(a, b, c)
+  a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && c + b > a
 end
 
 # Error class used in part 2.  No need to change this code.
